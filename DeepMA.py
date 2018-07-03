639c594b14365222e734df902bd061710aab606b
Xiaqiong <32009748+xiaqiong@users.noreply.github.com>
2018-07-03 21:56:03 +0800

    Update

--- DeepMA.py
+++ DeepMA.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 import datetime
 import os
 import csv
-#import model0
 
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
